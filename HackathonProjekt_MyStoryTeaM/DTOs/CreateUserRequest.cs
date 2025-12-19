0909273f66f8699747490e057930a54416618cde
Sansan2312 <fklasich@gmail.com>
2025-12-19 21:54:58 +0100

    Update

--- HackathonProjekt_MyStoryTeaM/DTOs/CreateUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HackathonProjekt_MyStoryTeaM.DTOs
-{
-    public class CreateUserRequest
-    {
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
-
-}
